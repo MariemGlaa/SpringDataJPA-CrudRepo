bf6045d10332ca325bf296da531f177a0ebdefee
Dhia.BenMabrouk <dhia.benmabrouk@esprit.tn>
2022-11-16 21:26:49 +0100

    final

---
+++ src/test/java/tn/esprit/rh/achat/services/FournisseurServiceTest.java
@@ -0,0 +1,34 @@
+package tn.esprit.rh.achat.services;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import tn.esprit.rh.achat.entities.CategorieFournisseur;
+import tn.esprit.rh.achat.entities.Fournisseur;
+
+@SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class FournisseurServiceTest {
+    
+    @Autowired
+    private IFournisseurService fs;
+    
+
+    /*@Test
+    @Order(1)
+    public void testaddFournisseur() {
+        Fournisseur fournisseur = Fournisseur.builder()
+					.code("new")
+					.libelle("should persist")
+					.categorieFournisseur(CategorieFournisseur.CONVENTIONNE).build();
+        assertNotNull(fs.addFournisseur(fournisseur));
+
+    }*/
+    
+    
+}
